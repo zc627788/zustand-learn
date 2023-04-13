bf7174ec117eab36152bb6d2ec823a128b120b10
jackson <14552087922@qq.com>
2023-04-13 18:32:53 +0800

    fix:这有一些优点:它不需要钩子来调用一个动作;它有助于代码分割。虽然这种模式没有任何缺点，但由于其封装的性质，有些人可能更喜欢重新组合。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import {  useBearStore } from "../store";
-import { shallow } from "zustand/shallow";
+import { useBoundStore, inc, setText } from "../store";
 
 // In consuming app
 export default function App() {
-
-
   //省略写法
-  const bears = useBearStore.use.bears();
-  const increase = useBearStore.use.increment();
+  const count = useBoundStore((state: any) => state.count);
+  const text = useBoundStore((state: any) => state.text);
+
+
 
   return (
     <main>
-      <p>bear:{bears}</p>
-      <button onClick={increase}>增加熊</button>
+      <p>bear:{count}</p>
+      <button onClick={inc}>增加熊</button>
+      <input type="text" onChange={setText} value={text} />
+      <p>{text}</p>
     </main>
   );
 }
--- store/index.tsx
+++ store/index.tsx
@@ -1,88 +1,23 @@
-import produce from "immer";
-import { create, StoreApi, UseBoundStore } from "zustand";
+import { create } from "zustand";
+// 组合式
+// export const useBoundStore = create((set) => ({
+//   count: 0,
+//   text: "hello",
+//   inc: () => set((state: any) => ({ count: state.count + 1 })),
+//   setText: (text: any) => set({ text }),
+// }));
 
-type WithSelectors<S> = S extends { getState: () => infer T }
-  ? S & { use: { [K in keyof T]: () => T[K] } }
-  : never;
-//官方配置,不需要引入hook,直接用 
-// get the property
-// const bears = useBearStore.use.bears()
-
-const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
-  _store: S
-) => {
-  let store = _store as WithSelectors<typeof _store>;
-  store.use = {};
-  for (let k of Object.keys(store.getState())) {
-    (store.use as any)[k] = () => store((s) => s[k as keyof typeof s]);
-  }
-
-  return store;
-};
-
-type State = {
-  firstName: string;
-  lastName: string;
-  deep: {
-    nested: {
-      obj: { count: number };
-    };
-  };
-};
-
-interface BearState {
-  bears: number;
-  increase: (by: number) => void;
-  increment: () => void;
-}
-
-type Action = {
-  updateFirstName: (firstName: State["firstName"]) => void;
-  updateLastName: (lastName: State["lastName"]) => void;
-  updateDeep: (count: any) => void;
-};
-
-// Create your store, which includes both state and (optionally) actions
-export const useStore = create<State & Action>((set) => ({
-  firstName: "",
-  lastName: "",
-  deep: {
-    nested: {
-      obj: { count: 0 },
-    },
-  },
-  //如果set后面设置为true禁用合并行为
-  // set内部自己合并了状态 不需要在{...state,firstName:firstName}
-  updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
-  updateLastName: (lastName) => set(() => ({ lastName: lastName })),
-  // 如果是嵌套对象,出现了什么问题?
-  // 需要你嵌套很多层去记录之前的状态
-  // updateDeep: (count) =>
-  //   set((state) => ({
-  //     deep: {
-  //       ...state.deep,
-  //       nested: {
-  //         ...state.deep.nested,
-  //         obj: {
-  //           ...state.deep.nested.obj,
-  //           count: count,
-  //         },
-  //       },
-  //     },
-  //   })),
-  // 使用immer,解决繁杂的嵌套写法
-  updateDeep: (count) =>
-    set(
-      produce((state: any) => {
-        state.deep.nested.obj.count = count;
-      })
-    ),
+// 分离封装式
+// 值和逻辑处理分开
+export const useBoundStore = create(() => ({
+  count: 0,
+  text: "hello",
 }));
 
-const useBearStoreBase = create<BearState>()((set) => ({
-  bears: 0,
-  increase: (by) => set((state) => ({ bears: state.bears + by })),
-  increment: () => set((state) => ({ bears: state.bears + 1 })),
-}));
+export const inc: any = () =>
+  useBoundStore.setState((state) => ({ count: state.count + 1 }));
 
-export const useBearStore = createSelectors(useBearStoreBase);
+export const setText = (e: any) => {
+  console.log(e.target.value);
+  useBoundStore.setState(() => ({ text: e.target.value}));
+};
